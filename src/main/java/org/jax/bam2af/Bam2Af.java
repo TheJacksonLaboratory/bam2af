f5b3c1ce350d264472adfc34083156647a860b32
pnrobinson <peter.robinson@jax.org>
2018-09-06 23:10:19 -0400

    adding prototype MD parser.

--- src/main/java/org/jax/bam2af/Bam2Af.java
+++ src/main/java/org/jax/bam2af/Bam2Af.java
@@ -1,13 +1,12 @@
 package org.jax.bam2af;
 
 import htsjdk.samtools.*;
+import org.jax.bam2af.analysis.IntervalAlleleCounter;
 import org.jax.bam2af.bam.SingleEndBamReader;
 import org.jax.bam2af.bed.BedParser;
 import org.jax.bam2af.bed.GenomicInterval;
 import org.jax.bam2af.exception.Bam2AfException;
 
-import java.io.File;
-import java.util.Iterator;
 import java.util.List;
 
 public class Bam2Af {
@@ -20,8 +19,8 @@ public class Bam2Af {
 
 
     public static void main(String args[]) {
-        String bamSample="scripts/sorted_toy_aln.bam";
-        String bedSample="src/test/resources/bam/to_intervals.bed";
+        String bamSample="src/test/resources/bam/sorted_toy_aln.bam";
+        String bedSample="src/test/resources/bam/toy_intervals.bed";
         // note--need to make index first: samtools index SRR6350627_sorted.bam
         Bam2Af b2f=new Bam2Af(bamSample,bedSample);
         b2f.parseBam();
@@ -53,16 +52,13 @@ public class Bam2Af {
             int from = gi.getFromPosition();
             int to = gi.getToPosition();
             SAMRecordIterator iter = seBamReader.getRecordsInInterval(chrom,from,to);
+            IntervalAlleleCounter iac = new IntervalAlleleCounter(iter,chrom,from,to);
         }
 
 
         SAMRecordIterator iter = seBamReader.getGlobalIterator();    //query("my_chromosome", 0, 0, false);
 
         while(iter.hasNext()){
-            if (iter==null) {
-                System.err.println("item null");
-                continue;
-            }
             // Iterate thorough each record and extract fragment size
             final SAMRecord rec= iter.next();
             int tlen= rec.getInferredInsertSize();
--- src/main/java/org/jax/bam2af/analysis/IntervalAlleleCounter.java
+++ src/main/java/org/jax/bam2af/analysis/IntervalAlleleCounter.java
@@ -1,8 +1,7 @@
 package org.jax.bam2af.analysis;
 
-import htsjdk.samtools.SAMRecord;
-import htsjdk.samtools.SAMRecordIterator;
-import org.apache.spark.sql.sources.In;
+import htsjdk.samtools.*;
+import org.jax.bam2af.exception.Bam2AfException;
 
 /**
  * The purpose of this class is to count the alleles present in the columns of an alignment
@@ -12,7 +11,7 @@ import org.apache.spark.sql.sources.In;
  */
 public class IntervalAlleleCounter {
 
-    enum Altbase {ALTBASE_A,ALTBASE_C,ALTBASE_G,ALTBASE_T,ALTBASE_N}
+    enum Altbase {ALTBASE_A, ALTBASE_C, ALTBASE_G, ALTBASE_T, ALTBASE_N}
 
 
     private final String chromosome;
@@ -21,24 +20,42 @@ public class IntervalAlleleCounter {
 
     private final int toPosition;
 
-    private ColumnCounter[] columns;
-
+    private final ColumnCounter[] columns;
 
 
     public IntervalAlleleCounter(SAMRecordIterator iter, String chrom, int from, int to) {
-        chromosome=chrom;
-        fromPosition=from;
-        toPosition=to;
-        int len = from-to+1;
-        columns=new ColumnCounter[len];
-        processReads(iter);
-    }
-
-
-
-    private void processReads(SAMRecordIterator iter) {
+        chromosome = chrom;
+        fromPosition = from;
+        toPosition = to;
+        int len = to - from + 1;
+        columns = new ColumnCounter[len];
         while (iter.hasNext()) {
-            SAMRecord record = iter.next();
+            try {
+                processRead(iter.next());
+            } catch (Bam2AfException e) {
+                e.printStackTrace();
+            }
         }
     }
+
+
+    private void processRead(SAMRecord record) throws Bam2AfException {
+        final String md = record.getStringAttribute(SAMTag.MD.name());
+        if (md == null) {
+            throw new Bam2AfException("Cannot create reference from SAMRecord with no MD tag, read: " + record.getReadName());
+        }
+        // Not sure how long output will be, but it will be no longer than this.
+        // int maxOutputLength = 0;
+        final Cigar cigar = record.getCigar();
+        if (cigar == null) {
+            throw new SAMException("Cannot create reference from SAMRecord with no CIGAR, read: " + record.getReadName());
+        }
+        int maxOutputLength = 0;
+        for (final CigarElement cigarElement : cigar.getCigarElements()) {
+            maxOutputLength += cigarElement.getLength();
+        }
+        System.out.println("len="+maxOutputLength+" MD="+md);
+    }
+
+
 }
